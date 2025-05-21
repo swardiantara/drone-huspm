5e55dc2e510a140de381f0481941035025dcbab1
swardiantara <swardyantara@gmail.com>
2025-05-21 12:12:44 +0700

    test log abstraction model

--- SOLAD_cli.py
+++ SOLAD_cli.py
@@ -1,9 +1,11 @@
 from typing import List, Dict, Tuple
 import os
+import joblib
 import torch
 import logging
 from src.data_loader import DataLoader
 from src.adfler import MessageSegmenter
+from src.lasec import LogAbstractor
 from src.utils import get_latest_folder
 import json
 from dataclasses import asdict
@@ -15,12 +17,10 @@ logger = logging.getLogger(__name__)
 class DroneLogAnalyzer:
     def __init__(self, config: Dict):
         # Initialize all components from config
+        self.config = config
         self.data_loader = DataLoader(config['data_path'])
         self.segmenter = MessageSegmenter(config['ner_model_path'], use_cuda=config['use_cuda'])
-        # self.abstractor = LogAbstractor(
-        #     config['embedding_model_path'],
-        #     config.get('birch_model_path')
-        # )
+        self.abstractor = self._load_lasec()
         # self.detector = AnomalyDetector(
         #     config['severity_model_path'],
         #     config['classifier_path']
@@ -30,7 +30,19 @@ class DroneLogAnalyzer:
         #     config['classifier_path']
         # )
         # self.seq_db_builder = SequenceBuilder()
-    
+
+    def _load_lasec(self):
+        file_path = os.path.join(self.config['workdir'], 'LASeC.joblib')
+
+        if os.path.exists(file_path):
+            self.abstractor = joblib.load(file_path)
+        else:
+            self.abstractor = LogAbstractor(
+                self.config['embedding_model_path'],
+                self.config['device'],
+                self.config.get('birch_model_path')
+            )
+
     def analyze(self):
         """Run the complete analysis pipeline"""
         # 1. Load data
@@ -42,8 +54,9 @@ class DroneLogAnalyzer:
         records = self.segmenter.segment_and_classify(records)
         
         # # 3. Abstract events
-        # records = self.abstractor.abstract_messages(records)
-        
+        records = self.abstractor.abstract_messages(records)
+        joblib.dump(self.abstractor, os.path.join(self.config['workdir'], 'LASeC.joblib'))
+
         # # 4. Detect anomalies
         # records = self.detector.detect_anomalies(records)
         
@@ -59,6 +72,7 @@ class DroneLogAnalyzer:
 
 def main():
     use_cuda = True if torch.cuda.is_available() else False
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     parsed_folder = get_latest_folder('outputs')
     source_path = os.path.join('outputs', parsed_folder, 'parsed', 'android')
     files = os.listdir(source_path)
@@ -70,11 +84,13 @@ def main():
         logger.info(f'Processing file: {full_path}')
 
         config = {
+            'work_dir': parsed_folder,
+            'use_cuda': use_cuda,
+            'device': device,
             'data_path': full_path,
             'ner_model_path': 'ADFLER-albert-base-v2',
-            'use_cuda': use_cuda
-            # 'embedding_model_path': 'all-MiniLM-L6-v2',
-            # 'birch_model_path': 'birch_model.pkl',
+            'embedding_model_path': 'swardiantara/drone-sbert',
+            'birch_model_path': os.path.join(parsed_folder, 'birch_model.joblib'),
             # 'severity_model_path': 'severity_model',
             # 'classifier_path': 'classifier.pt',
             # 'attribution_model_path': 'attribution_model'
---
+++ src/lasec.py
@@ -0,0 +1,56 @@
+import joblib
+from data_loader import LogRecord
+from typing import List, Dict, Tuple
+from sklearn.cluster import Birch
+from sentence_transformers import SentenceTransformer
+
+class LogAbstractor:
+    def __init__(self, model_path: str, device, birch_model_path: str = None):
+        self.birch_model_path = birch_model_path
+        self.embedding_model = SentenceTransformer(model_path, device=device)
+        self.birch_model = self._load_birch_model(birch_model_path)
+        self.cluster_members = Dict[int, List[str]]
+        
+    def _load_birch_model(self, path):
+        if path:
+            # Load pre-trained BIRCH model
+            return joblib.load(path)
+        else:
+            # Initialize new BIRCH model
+            return Birch(n_clusters=None, threshold=0.3)
+        
+    def _save_birch_model(self):
+        joblib.dump(self.birch_model, self.birch_model_path)
+    
+    def abstract_messages(self, records: List[LogRecord]) -> List[LogRecord]:
+        """Assign event IDs to each sentence via semantic clustering"""
+        # Collect all sentences for batch processing
+        all_sentences = []
+        sentence_refs = []  # To map back to original records
+        
+        for i, record in enumerate(records):
+            for j, sentence in enumerate(record.sentences):
+                all_sentences.append(sentence)
+                sentence_refs.append((i, j))
+        
+        # Generate embeddings
+        embeddings = self.embedding_model.encode(all_sentences, normalize_embeddings=True)
+        
+        # Cluster (or predict if pre-trained)
+        if self.birch_model.n_clusters is None:
+            clusters = self.birch_model.fit_predict(embeddings)
+        else:
+            clusters = self.birch_model.predict(embeddings)
+        
+        # Assign event IDs back to records
+        for (i, j), cluster_id in zip(sentence_refs, clusters):
+            sentence_type = records[i].sentence_types[j]
+            prefix = sentence_type[0] #'E' if sentence_type == 'event' else 'N'
+            event_id = f"{prefix}{cluster_id}"
+            
+            records[i].eventIds.append(event_id)
+            self.cluster_members[cluster_id].append(records[i].sentences[j])
+        # Save the last state for online setting
+        self._save_birch_model(self.birch_model_path)
+
+        return records
