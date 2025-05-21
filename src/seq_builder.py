af8ec817aac7f48f3cf344023ab64e0dd9314a3e
swardiantara <swardyantara@gmail.com>
2025-05-21 17:48:22 +0700

    left behind

---
+++ src/seq_builder.py
@@ -0,0 +1,30 @@
+from typing import List, Dict, Tuple
+from src.data_loader import LogRecord
+
+class SequenceBuilder:
+    def __init__(self, utility: str):
+        self.utility = utility
+
+    def build_sequences(self, records: List[LogRecord]) -> Dict:
+        """Construct the final sequential database"""
+        sequence = []
+
+        event_count = 0
+        for record in records:
+            element = []
+            if not record.eventIds:
+                continue
+            
+            for i in range(len(record.sentences)):
+                event_count += 1
+                element.append((record.eventIds[i]), record[self.utility][i])
+            
+            sequence.append(element)
+        
+        return {
+            'metadata': {
+                'total_records': len(records),
+                'total_event': event_count
+            },
+            'sequence': sequence
+        }
